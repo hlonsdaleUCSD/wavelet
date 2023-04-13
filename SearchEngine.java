07340655f38abbed06087aed8359710aa03a9c81
hlonsdaleUCSD <hlonsdale@ucsd.edu>
2023-04-13 11:28:46 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,3 +1,41 @@
-public class SearchEngine {
-    
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // String array of the strings to keep track of
+    ArrayList<String> strs = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("List of strings:", strs.toString());
+        } else if (url.getPath().equals("/clear")) {
+            strs.clear();
+            return String.format("All strings cleared");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    strs.add(parameters[1]);
+                    String ret = parameters[1] + " added! New list of strings: " + strs.toString();
+                    return ret;
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
 }
